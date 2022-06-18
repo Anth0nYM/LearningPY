9cb5257c0a74593090e3e4ba4f07737dc77b54b8
Anth0nYM <marquesanthony62@gmail.com>
2022-06-18 18:34:38 -0300

    atividade 02 sobre erros e excessões

--- taskk4.py
+++ taskk4.py
@@ -3,4 +3,20 @@ try:
     for i in ['a','b','c']:
         print(i**2)
 except TypeError:
-    print('Tipo de dado inválido')
+   print('Tipo de dado inválido')
+    
+#Manuseie a exceção gerada pelo código abaixo usando os blocos try e except. 
+#Em seguida, use um bloco finally para imprimir 'All Done'.'''
+try:
+    x =int(input('Digite um número: '))
+    y =int(input('Digite outro número: '))
+    z = x/y
+except ZeroDivisionError:
+    print('Não é possível dividir por zero')
+except TypeError:
+    print('Por favor digite apenas números')
+else:
+    print(z)
+finally:
+    print('All Done')    
+
